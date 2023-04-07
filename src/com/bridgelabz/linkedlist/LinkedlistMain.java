2a69db8f10e10d5ff2bb88ef7143534cf865dc6d
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-04-07 17:30:46 +0530

    Add : Able to add elemnts into the linkedlist

--- src/com/bridgelabz/linkedlist/LinkedlistMain.java
+++ src/com/bridgelabz/linkedlist/LinkedlistMain.java
@@ -4,9 +4,9 @@ public class LinkedlistMain {
 
 	public static void main(String[] args) {
 		Linkedlist list = new Linkedlist();
-		list.add(56);
-		list.add(30);
 		list.add(70);
+		list.add(30);
+		list.add(56);
 		list.printList();
 
 	}
